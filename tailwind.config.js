9c63fa921580afc9fc3042f27c5a91dc124b7b80
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-05-18 11:21:30 +0200

    font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        archivo: ['Archivo', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+    },
   },
+  plugins: [require('daisyui')],
   daisyui: {
     themes: false,
   },
-  plugins: [require('daisyui')],
 };
