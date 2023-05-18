f541636c6244365d447dd17d7e154425f243bc76
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-05-18 14:24:54 +0200

    primary desgin for navbar done

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../shared/ErrorPage/Navbar/Navbar';
 
 const MainLayout = () => {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-center text-3xl font-bold">
-        Welcome to the React App
-      </h1>
-      <Outlet />
+    <div className="">
+      <Navbar />
+      <div className="container mx-auto">
+        <Outlet />
+      </div>
     </div>
   );
 };
---
+++ src/shared/ErrorPage/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import { Link, NavLink } from 'react-router-dom';
+import logo from '/logo-no-background.png';
+
+const Navbar = () => {
+  return (
+    <nav className=" bg-[#f8f9fa] shadow-md sticky rounded-b-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <ul className="">
+          <li>
+            <Link to="/">
+              <img className="max-w-full h-12 md:h-16" src={logo} alt="" />
+            </Link>
+          </li>
+        </ul>
+
+        <ul className="flex flex-col md:flex-row justify-center gap-3">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? `border-b border-b-[#0077b6] text-[#00b4d8] p-1`
+                  : `border-0 text-[#00b4d8] p-1`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/blog"
+              className={({ isActive }) =>
+                isActive
+                  ? `border-b border-b-[#0077b6] text-[#00b4d8] p-1`
+                  : `border-0 text-[#00b4d8] p-1`
+              }
+            >
+              Blog
+            </NavLink>
+          </li>
+        </ul>
+        <ul className="hidden md:flex gap-2">
+          <li>sign-in</li>
+          <li>sign-in</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
